e4ff4c3bfb47e954d5ed602d372684452db893d3
Rob Parker <into.the.parker@gmail.com>
2018-08-30 13:38:49 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,24 @@
+def reverse_each_word(string)
+  
+  array = string.split(' ')
+  rev_string = []
+  rev_return = []
+  
+  array.each do |rev|
+     rev_string.push(rev)
+  end
+  
+  
+      rev_string.each do |ver|
+         rev_return.push(ver.reverse)
+      end  
+    
+  
+      rev_string.collect do |ver|
+         ver.reverse
+      end  
+    
+rev_return.join(' ')
+end
+
+
