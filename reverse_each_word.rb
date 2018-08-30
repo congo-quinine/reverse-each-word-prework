69da1deded909ffcd349bb1f3714f842e4af5249
Rob Parker <into.the.parker@gmail.com>
2018-08-30 13:30:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,3 +14,21 @@ def reverse_each_word(string)
     
 rev_return.join(' ')
 end
+
+
+def reverse_each_word(string)
+  
+  array = string.split(' ')
+  rev_string = []
+  rev_return = []
+  
+  array.each do |rev|
+     rev_string.push(rev)
+  end
+  
+      rev_string.collect do |ver|
+         ver.reverse
+      end  
+    
+rev_return.join(' ')
+end
