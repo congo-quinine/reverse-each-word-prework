71dfa34d5c0e8fa4588f12b9cddebace551b3e73
Rob Parker <into.the.parker@gmail.com>
2018-08-30 13:28:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,15 +2,15 @@ def reverse_each_word(string)
   
   array = string.split(' ')
   rev_string = []
+  rev_return = []
   
   array.each do |rev|
      rev_string.push(rev)
   end
   
       rev_string.each do |ver|
-        ver.reverse
+         rev_return.push(ver.reverse)
       end  
     
-rev_string
-end 
-  
+rev_return.join(' ')
+end
